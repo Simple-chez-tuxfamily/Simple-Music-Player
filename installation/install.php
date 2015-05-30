7f2115b8303d72dc0a6fd34027d43ab17d3e9dc2
Quentin Bouteiller <quentin@quent1.fr>
2015-05-30 17:03:08 +0200

    Ajout de commentaire et suppression automatique
    Ajout de commentaires sur le script d'installation et suppression
    automatique du dossier installation/ en cas de succès.

--- installation/install.php
+++ installation/install.php
@@ -1,20 +1,30 @@
 <?php
     if(!empty($_POST['pseudo']) AND !empty($_POST['pwd1']) AND !empty($_POST['pwd2'])){
         if($_POST['pwd1'] == $_POST['pwd2']){
+			// On charge la base de données
             $sqlite = new PDO('sqlite:../core/private/data.db');
         
+			// On sanitize l'entrée utilisateur
 		    $user = $sqlite->quote($_POST['pseudo']);
             $pass = $sqlite->quote(sha1($_POST['pseudo'] . $_POST['pwd1'] . $_POST['pseudo']));
-		
+			
+			// On vide la table « users » puis on créé un premier utilisateur
 			$sqlite->query('DELETE FROM users');
             $sqlite->query('INSERT INTO users (username, password, admin) VALUES(' . $user . ',' . $pass . ', "1")');
             
+			// On créé les dossiers de l'utilisateur
 			mkdir('../data');
 			mkdir('../data/' . $_POST['pseudo']);
 		    mkdir('../data/' . $_POST['pseudo'] . '/pictures');
 			mkdir('../data/' . $_POST['pseudo'] . '/songs');
-	    
-            header('Location: index.php?ok');
+			
+			// On supprime les fichiers et le dossier d'installation
+			unlink('./index.php');
+			unlink('./install.php');
+			rmdir('../installation');
+			
+			// On redirige l'utilisateur vers la page de connexion
+            header('Location: ../index.php');
         }
         else
             header('Location: index.php?err');
