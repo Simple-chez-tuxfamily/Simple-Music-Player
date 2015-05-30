ea8cd59ccf5f828571a6a888d52da1df840cf9d7
Quentin Bouteiller <quentin@quent1.fr>
2015-05-30 16:53:25 +0200

    Typo
    Correction de petites erreurs d'indentation

--- installation/install.php
+++ installation/install.php
@@ -2,22 +2,22 @@
     if(!empty($_POST['pseudo']) AND !empty($_POST['pwd1']) AND !empty($_POST['pwd2'])){
         if($_POST['pwd1'] == $_POST['pwd2']){
             $sqlite = new PDO('sqlite:../core/private/data.db');
-            $user = $sqlite->quote($_POST['pseudo']);
+        
+		    $user = $sqlite->quote($_POST['pseudo']);
             $pass = $sqlite->quote(sha1($_POST['pseudo'] . $_POST['pwd1'] . $_POST['pseudo']));
-	    $sqlite->query('DELETE FROM users');
+		
+			$sqlite->query('DELETE FROM users');
             $sqlite->query('INSERT INTO users (username, password, admin) VALUES(' . $user . ',' . $pass . ', "1")');
             
-	    mkdir('../data/' . $_POST['pseudo']);
-	    mkdir('../data/' . $_POST['pseudo'] . '/pictures');
-	    mkdir('../data/' . $_POST['pseudo'] . '/songs');
+			mkdir('../data/' . $_POST['pseudo']);
+		    mkdir('../data/' . $_POST['pseudo'] . '/pictures');
+			mkdir('../data/' . $_POST['pseudo'] . '/songs');
 	    
             header('Location: index.php?ok');
         }
-        else{
+        else
             header('Location: index.php?err');
-        }
     }
-    else{
+    else
         header('Location: index.php?err');
-    }
 ?>
